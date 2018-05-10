75d6abf2a69f1f5bc9994c0fd9cdcc9b981d6365
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-05-10 12:29:11 +0300

    created UI elements and needed VC's

--- SortStatistic/MainViewController.swift
+++ SortStatistic/MainViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let containerView = storyboard.instantiateViewController(withIdentifier: "statisticTVC") as? StatisticTableViewController else { return }
+        self.navigationController?.pushViewController(containerView, animated: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
